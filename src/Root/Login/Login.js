051b06801463d5a11b6c7483843d517d7dbb4ee9
mclt0568 <tis7bfrankie@gmail.com>
2020-11-17 19:25:46 +0800

    rewrote login form to enable validation

--- src/Root/Login/Login.js
+++ src/Root/Login/Login.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Helmet} from 'react-helmet';
 import {makeStyles} from '@material-ui/styles'
-import {Formik, Form, Field} from 'formik' 
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import InputLabel from '@material-ui/core/InputLabel'
+import Input from '@material-ui/core/Input'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import FormControl from '@material-ui/core/FormControl'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import IconButton from '@material-ui/core/IconButton'
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 import imgHello from "./hello.png"
 
@@ -18,15 +24,94 @@ const styles = makeStyles({
 	"flexBox":{
 		"display":"flex",
 		"justify-content":"space-between"
+	},
+	"formControl":{
+		"display":"block",
+		"margin-bottom":"8px",
+	},
+	"txtInput":{
+		"width":"100%"
+	},
+	"button":{
+		"margin-top":"10px"
 	}
 });
 
-function LoginButton(props){
-	return <Button {...props} color="secondary" />
+function validateUsername(name){
+	// 0 for valid, 1 for empty, 2 for invalid email format
+	var isEmail = false;
+	var returnCode = 0;
+	if (name.trim() === "") returnCode = 1;
+	if (new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$").test(name)) isEmail = true;
+	if (!isEmail && new RegExp("@").test(name)) returnCode = 2;
+	return [(returnCode>0), returnCode];
 }
 
-function LoginInputs(props){
-	return <TextField {...props} color="secondary" />
+function validatePassword(password){
+	//0 for valid, 1 for empty
+	var returnCode = 0;
+	if (password === "") returnCode = 1;
+	return [(returnCode>0),returnCode];
+}
+
+function LoginForm(props){
+	const [unState, setUnState] = useState([false,""]);
+	const [pwState, setPwState] = useState([false,""]);
+	const [validState, setValidState] = useState(true);
+	const [showPassword, setShowPassword] = useState(false);
+	const unErrorMessages = ["","Username cannot be empty","Invalid Email Address or Username containing '@'"];
+	const pwErrorMessages = ["","Password cannot be empty"];
+	const updateUnField = (eArgs) => {
+		const result = validateUsername(eArgs.target.value)
+		setUnState([result[0],unErrorMessages[result[1]]]);
+		setValidState(!result[0]);
+	}
+	const updatePwField = (eArgs) => {
+		const result = validatePassword(eArgs.target.value)
+		setPwState([result[0],pwErrorMessages[result[1]]]);
+		setValidState(!result[0]);
+	}
+	const updateShowPw = (eArgs) => {
+		setShowPassword(!showPassword);
+	}
+	const onLoginClick = (eArgs) => {
+		var message = validState ? "Successfully logged in" : "One or Multiple fields invalid!";
+		alert(message);
+	}
+	const classes = styles();
+	return ([
+		<FormControl className={classes.formControl} >
+			<InputLabel error={unState[0]} htmlFor="username">Username</InputLabel>
+			<Input
+				error={unState[0]}
+				id="username"
+				type="text"
+				className={classes.txtInput}
+				onChange={updateUnField}
+			/>
+			<FormHelperText error={unState[0]} id="username-error-text">{unState[1]}</FormHelperText>
+		</FormControl>,
+		<FormControl className={classes.formControl}>
+			<InputLabel error={pwState[0]} htmlFor="password">Password</InputLabel>
+			<Input
+				id="password"
+				error={pwState[0]}
+				className={classes.txtInput}
+				type={showPassword ? 'text' : 'password'}
+				onChange={updatePwField}
+				endAdornment={
+					<InputAdornment position="end">
+					<IconButton aria-label="toggle password visibility" onClick={updateShowPw}>
+					{showPassword ? <Visibility /> : <VisibilityOff />}
+					</IconButton>
+					</InputAdornment>
+				}
+			/>
+			<FormHelperText error={pwState[0]} id="password-error-text">{pwState[1]}</FormHelperText>
+		</FormControl>,
+		<div className={classes.formControl} class="g-recaptcha" data-sitekey="6Lc1AtEZAAAAAEBVa1wvNDBt7VJtBKsXhBEVGGQa"/>,
+		<Button className={classes.button} variant="contained" color="secondary" onClick={onLoginClick}>Login</Button>
+	])
 }
 
 export default function Login(props){
@@ -40,14 +125,7 @@ export default function Login(props){
 			<Box className={classes.flexBox}>
 				<span>
 					<Box mt={3}>
-						<Formik onSubmit={(values, {setSubmitting}) => {window.location="App"}} validate={(i)=>{return null}} initialValues={{username:"",password:""}}>
-							{({submitForm, isSubmitting, touched, errors}) => {return([
-								<Box mb={2}><Field component={LoginInputs} name="username" type="username" label="Username / E-Mail"/></Box>,
-								<Box mb={2}><Field component={LoginInputs} name="password" type="password" label="Password"/></Box>,
-								<Box mb={2}><div class="g-recaptcha" data-sitekey="6Lc1AtEZAAAAAEBVa1wvNDBt7VJtBKsXhBEVGGQa"/></Box>,
-								<Box mb={2}><LoginButton variant="contained" onClick={submitForm}>Login</LoginButton></Box>,
-							]);}}
-						</Formik>
+					<LoginForm/>
 					</Box>
 				</span>
 				<span>
